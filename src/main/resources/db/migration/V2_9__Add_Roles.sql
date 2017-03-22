c2d7c99f9ebc3caad3c0208dfea42970f6e5afda
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-03-22 11:40:03 -0600

    finished migrations for non-join tables

---
+++ src/main/resources/db/migration/V2_9__Add_Roles.sql
@@ -0,0 +1,13 @@
+TRUNCATE TABLE roles;
+
+-- create roles
+INSERT INTO roles (title)
+VALUES
+  ('Schedule Admin'),
+  ('Bartender'),
+  ('Server'),
+  ('Host'),
+  ('Manager'),
+  ('Head Chef'),
+  ('Line Cook'),
+  ('Prep Cook')
