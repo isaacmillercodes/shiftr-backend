682614686ca7990a559ac87f44673d30c14da870
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-03-22 10:46:21 -0600

    users seeded

---
+++ src/main/resources/db/migration/V2_1__Add_Users.sql
@@ -0,0 +1,17 @@
+TRUNCATE TABLE users;
+
+-- create users
+INSERT INTO users (email, password)
+VALUES
+  ('vince@mcmahon.com', '$2a$06$uadMvAWv/CuyisavBs0L2Ow4SkiAdDMIRWMy2w6yJAVDEFsZomwNq'),
+  ('shane@mcmahon.com', '$2a$06$aGp8wEcapW2IGAIn.SGRwOdl7sstZb4oQW43Tk4/s/S7GXBk5odEK'),
+  ('stephanie@mcmahon.com', '$2a$06$x9fe4Nt2WKc2YV8uryCZ8ud7q3SruxFkmUr/LINrwa8v9VstpRcJK'),
+  ('dwayne@johnson.com', '$2a$06$JG48Fo4ScZkQpRDolz/WmO/SvCgP9CE2pVTS/jB3jjwOF6qXOaDo.'),
+  ('steve@austin.com', '$2a$06$LsYJQ.wCCV/eb24Iz/rM.OFu1zL63KXZbD4GukUXICEU5MDf7M9V6'),
+  ('mick@foley.com', '$2a$06$NBOfXLkw340Uo5J60Hy5VOxymiF749L/klSJf6kkfkaqYOxyBCHyy'),
+  ('randy@savage.com', '$2a$06$4iUjS9hUK4DRljgR4EfI9.Jp1qqcXV1OjgBzlELz/AFS0tdsLR18S'),
+  ('sasha@banks.com', '$2a$06$0JpohJ20Ykt.ZtFPo96TAOGyXVbVePXs59gG9aFKlDvQSckcQU5Ii'),
+  ('charlotte@flair.com', '$2a$06$tbwr182bN9lpeO1XX40bDuOyTkbYIIDS0d5CZrq40y0SBMd.uDzya'),
+  ('kevin@owens.com', '$2a$06$8MKLe1x9MzFADzOaHvpJqOtyNu.gnYpdZZ9A/JJue/slqAklNd7ze'),
+  ('finn@balor.com', '$2a$06$QD0z5jxOqnTr0OoIYSzxeucebQinw/q9i6A7t0qY01878ADhRnkZi'),
+  ('shinsuke@nakamura.com', '$2a$06$.6rywIH9Nws0r.2vDpsq9u.XCdbLylyQ1y51rJo4oloie.P98aCDq')
